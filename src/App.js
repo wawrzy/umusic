7596273bed47296b1d880664036424f47dd7c4d4
Julien Wawrzyniak <julien.wawrzyniak@epitech.eu>
2018-09-14 16:06:35 +0200

    Configuration material-ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { MuiThemeProvider } from '@material-ui/core/styles';
 import store from './store';
+import theme from './theme';
 
 const App = () => (
   <Provider store={store}>
-    <div>App</div>
+    <MuiThemeProvider theme={theme}>
+      <div>App</div>
+    </MuiThemeProvider>
   </Provider>
 );
 
