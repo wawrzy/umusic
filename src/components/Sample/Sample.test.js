9e594d0eeef7a5f3e8a1550fa26f23d0d5a313b3
Julien Wawrzyniak <julien.wawrzyniak@epitech.eu>
2018-09-16 18:12:38 +0200

    Configuration travis deploy heroku

--- src/components/Sample/Sample.test.js
+++ src/components/Sample/Sample.test.js
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer';
 import Sample from './Sample';
 
 it('should renders without ui change', () => {
-  const component = renderer.create(<Sample name="saple" />);
+  const component = renderer.create(<Sample name="sample" />);
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
